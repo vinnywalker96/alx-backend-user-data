5677bc5234a4b4d7321add904f20209bffe728fb
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-17 13:58:38 +0200

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,18 @@ class BasicAuth(Auth):
     """
     BasicAuth inherit from auth
     """
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+            ) -> str:
+        """
+        returns Base64 part of the Authorization header
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        res = authorization_header.split(" ")[1]
+        return res
