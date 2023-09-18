f1687be3472a8c9b98c57a64cb4b8d9451f2ecc5
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-18 14:38:21 +0200

    Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,13 +9,14 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth = BasicAuth()
+auth = SessionAuth()
 
 excluded_paths = [
         '/api/v1/status/',
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Sessions"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Inherit from base Auth"""
+
