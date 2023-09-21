f200009bb352b8ab01c5c05ae410bdaa9b44fa6c
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-21 09:10:19 +0200

    hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Hash password"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashed password
+
+    Args:
+        password: string to be hashed
+
+    Returns:
+        Hashed password
+    """
+    salt = bcrypt.gensalt()
+    psswd = password.encode('utf-8')
+    return bcrypt.hashpw(psswd, salt)`L
