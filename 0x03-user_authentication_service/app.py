26e597410a6a3150a76295c09dd74e503e084761
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-21 13:19:15 +0200

    Credentials Validation

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python3
 """Basic flask app"""
-from flask import Flask, jsonify
+from flask import (
+        Flask,
+        jsonify,
+        request
+        )
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
 def home():
     """Basic flask app"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users/", methods=["POST"], strict_slashes=False)
+def register_user():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify(
+                {"message": "email already registered"}
+                ), 400
+    return jsonify({"email": f"{email}", "message": "user created"})
 
 
 if __name__ == "__main__":
