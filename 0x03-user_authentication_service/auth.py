0b98a63ad4a8a31dce9bbee5fcbb165864b77995
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-22 08:48:33 +0200

    Find user by id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -101,7 +101,7 @@ class Auth:
         except NoResultFound:
             return None
 
-    def destroy_session(self, user_id: str) -> None:
+    def destroy_session(self, user_id: int) -> None:
         """Detroys Session
 
         Args:
