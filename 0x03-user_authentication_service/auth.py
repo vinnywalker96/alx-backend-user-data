d1ead0bd06e65c461c3272833c34d3c37f21d720
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-22 10:45:04 +0200

    added reset_token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -118,9 +118,11 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """Resets password"""
-        user = self._db.find_user_by(email=email)
-        if not user:
-            raise ValueError()
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
         reset_token = _generate_uuid()
-        user.reset_token = reset_token
+        self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
