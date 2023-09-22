559c95cc3ded52a1a75c6ff75a203a18a19c3c31
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-22 08:35:08 +0200

    Find user by id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -107,8 +107,6 @@ class Auth:
         Args:
             user_id: id for the user
         """
-        if user_id is None:
-            return None
         try:
             user = self._db.find_user_by(user_id, session_id=None)
         except ValueError:
